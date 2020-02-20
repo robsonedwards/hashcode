240c49d4fdcf3e3bd3bdaa5d1e3861a1c52403ee
Robson Edwards <robson.edwards@gmail.com>
2020-02-20 20:20:20 +0000

    libraries know their id number now

--- a_soln.py
+++ a_soln.py
@@ -1,11 +1,15 @@
 #python 3.6
 class Library:
     def __init__(self, num_books = 0, signup_days = 0, shipping_per_day = 0,
-                 book_ids = set()):
+                 book_ids = set(), id = -1):
         self.num_books = num_books
         self.signup_days = signup_days
         self.shipping_per_day = shipping_per_day
         self.book_ids = book_ids
+        self.id
+
+    def __str__(self):
+        return "Library: TODO"
 
 INPUT = "a_example.txt"
 
@@ -23,6 +27,7 @@ while i < len(lines):
     library = Library()
     library.num_books, library.signup_days, library.shipping_per_day = (
         int(x) for x in line.split(" "))
+    library.id = i - 2
     
     i += 1
     line = lines[i]
@@ -36,4 +41,4 @@ while i < len(lines):
 print("B: ", B, ", D: ", D, ", S: ", S, sep = "")
 for library in libraries:
     print(library.num_books, library.signup_days, library.shipping_per_day,
-          library.book_ids)
+          library.book_ids, library.id)
