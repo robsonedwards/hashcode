77653fe5e18a188d865883ed76d9f21f9ff35634
Robson Edwards <robson.edwards@gmail.com>
2020-02-20 18:47:06 +0000

    parser done?

--- a_soln.py
+++ a_soln.py
@@ -1,18 +1,38 @@
 #python 3.6
 
+class Library:
+    num_books = 0
+    signup_days = 0
+    shipping_per_day = 0
+    book_ids = set()
+
 INPUT = "a_example.txt"
 
-f = open(INPUT, "r")
 
-line = f.readline()
-B, L, D = line.split(" ") # number of books, libraries, and days, respectively
+with open(INPUT, "r") as f:
+    lines = f.read().splitlines() # removes \n, probably better way to do this
 
-line = f.readline()
-S = line.split(" ") # array of book scores (books 0 to B-1)
+B, L, D = lines[0].split(" ") # number of books, libraries, and days, respectively
 
-line = f.readLine()
-while line is not None:
+S = lines[1].split(" ") # array of book scores (books 0 to B-1)
+
+libraries = []
+i = 2
+while i < len(lines):
     # read in info on each of the L libraries 0 to L-1
-    line = f.readLine()
-    pass
+    
+    line = lines[i]
+    library = Library()
+    library.num_books, library.signup_days, library.shipping_per_day = line.split(" ")
+    
+    i += 1
+    line = lines[i]
+    for book in line.split(" "):
+        library.book_ids.add(book)
+    
+    libraries.append(library)
+
+    i += 1
+    
+print(B, L, D, S, libraries)
 
