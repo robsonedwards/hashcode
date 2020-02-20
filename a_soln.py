b04f7c04b08e992a8fdeb84a8d4c539347317d7a
Robson Edwards <robson.edwards@gmail.com>
2020-02-20 18:26:00 +0000

    working on solution to a

--- a_soln.py
+++ a_soln.py
@@ -1,9 +1,18 @@
+#python 3.6
+
 INPUT = "a_example.txt"
 
 f = open(INPUT, "r")
 
 line = f.readline()
-B, L, D = line.split(" ")
-print(B, L, D)
-#while line is not None:
-#    pass
+B, L, D = line.split(" ") # number of books, libraries, and days, respectively
+
+line = f.readline()
+S = line.split(" ") # array of book scores (books 0 to B-1)
+
+line = f.readLine()
+while line is not None:
+    # read in info on each of the L libraries 0 to L-1
+    line = f.readLine()
+    pass
+
